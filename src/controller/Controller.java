439344f6921ecb0983c986509fdda21f83188386
Vineet Dhanawat <vineetdhanawat@gmail.com>
2014-04-30 12:22:12 -0500

    Initial Commit

---
+++ src/controller/Controller.java
@@ -0,0 +1,66 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Controller
+{
+	public static int duration;
+
+	public static void main(String args[])
+	{
+		// User will let the node know its nodeID
+		// controller duration &
+		if (args.length > 0)
+		{
+			try
+			{
+				duration = 1000*Integer.parseInt(args[0]);
+				System.out.println("duration:"+duration);
+		    }
+			catch (Exception e)
+			{
+				System.err.println("Argument must be in proper format");
+				System.exit(1);
+		    }
+		}
+	}
+	
+	 // Reading File
+    void readFile()
+    {
+    	try
+    	{
+    		String str = "input_"+nodeID+".txt";
+    		BufferedReader ReadFile = new BufferedReader(new FileReader(str));
+    		while((str = ReadFile.readLine()) != null)
+    		{
+    			System.out.println("File Data:"+str);
+    		}
+        }
+        catch(Exception e)
+        {
+            System.out.println(e + " in readFile()");
+        }
+    }
+    
+    // Writing File
+    void writeFile()
+    {
+    	try
+    	{                              
+    		String str = message;
+    		String filePath = "output_"+nodeID+".txt";
+    		BufferedWriter WriteFile = new BufferedWriter(new FileWriter(filePath,true));
+    		WriteFile.write(str);
+    		WriteFile.write("\n");
+    		WriteFile.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e + " in writeFile()");
+        }
+    }
+}
---
+++ src/machine/Node.java
@@ -0,0 +1,77 @@
+package machine;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Node
+{
+	public static int nodeID;
+	public static int outgoingNodeID;
+	public static int duration;
+	public static String message;
+
+	public static void main(String args[])
+	{
+		// User will let the node know its nodeID
+		// node 9 100 5 "this is a message" &
+		if (args.length > 0)
+		{
+			try
+			{
+				nodeID = Integer.parseInt(args[0]);
+				duration = 1000*Integer.parseInt(args[1]);
+				outgoingNodeID = Integer.parseInt(args[2]);
+				message = args[3];
+				System.out.println("nodeID:"+nodeID);
+				System.out.println("duration:"+duration);
+				System.out.println("outgoingNodeID:"+outgoingNodeID);
+				System.out.println("message:"+message);
+		    }
+			catch (Exception e)
+			{
+				System.err.println("Argument must be in proper format");
+				System.exit(1);
+		    }
+		}
+	}
+	
+    // Reading File
+    void readFile()
+    {
+    	try
+    	{
+    		String str = "input_"+nodeID+".txt";
+    		BufferedReader ReadFile = new BufferedReader(new FileReader(str));
+    		while((str = ReadFile.readLine()) != null)
+    		{
+    			System.out.println("File Data:"+str);
+    		}
+        }
+        catch(Exception e)
+        {
+            System.out.println(e + " in readFile()");
+        }
+    }
+    
+    // Writing File
+    void writeFile()
+    {
+    	try
+    	{                              
+    		String str = message;
+    		String filePath = "output_"+nodeID+".txt";
+    		BufferedWriter WriteFile = new BufferedWriter(new FileWriter(filePath,true));
+    		WriteFile.write(str);
+    		WriteFile.write("\n");
+    		WriteFile.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e + " in writeFile()");
+        }
+    }
+    
+    
+}
